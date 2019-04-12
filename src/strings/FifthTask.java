aa77fe4c7d7ce1a2c4621da23370b832044952f3
Puffle5 <elena_haruno@abv.bg>
2019-04-12 20:20:53 +0300

    Remove blank lind.

--- src/strings/FifthTask.java
+++ src/strings/FifthTask.java
@@ -11,7 +11,6 @@ long[] fibonacciNumbers = new long[index];
 
 fibonacciNumbers[0] = 0; fibonacciNumbers[1] = 1;
 if (index > 2)
-
 {
 for (int i = 2; i < index; i++) {fibonacciNumbers[i] = fibonacciNumbers[i - 1] + fibonacciNumbers[i - 2];}
 System.out.println(fibonacciNumbers[index-1]);
